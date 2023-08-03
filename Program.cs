b8b7799956e44c0e3bea25f8b84acb0312c8df62
radu-andrei-oprea <ouser99@icloud.com>
2023-08-03 16:09:17 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using Squirrel;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seventh change:");
+            const string Value = "Moved to VS 2022";
+            Console.WriteLine(Value);
+            Console.WriteLine("\nPress any key to close...");
+            _ = Console.ReadKey();
+        }
+    }
+}
