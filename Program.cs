5b44434bb912510fff614d09cc11c8604d22d538
radu-andrei-oprea <ouser99@icloud.com>
2023-08-08 12:25:18 +0300

    -fixed links

--- Program.cs
+++ Program.cs
@@ -1,22 +1,28 @@
 ﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 using AutoUpdaterDotNET;
 
 namespace TestApp
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            // generic link to latest release
+            const String latestURL = $"https://api.github.com/radu-andrei-oprea/TestAppUpdates/releases/latest/download/version.xml";
 
-            const String updateUrl = "https://github.com/radu-andrei-oprea/TestAppUpdates/releases/tag/Latest/version.xml";
-            AutoUpdater.Start(updateUrl);
+            AutoUpdater.Start(latestURL);
 
-
-            Console.WriteLine("Eight change:");
-            const string Value = "Added AutoUpdater components";
+            // print text
+            Console.WriteLine("Ninth change:");
+            const string Value = "AutoUpdate prelude";
             Console.WriteLine(Value);
             Console.WriteLine("\nPress any key to close...");
             _ = Console.ReadKey();
+            
+            
+            //AutoUpdater.Start(updateUrl);
         }
 
        
