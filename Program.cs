2f89d2283a7df7a441a24b39fec6dbf74568fde0
radu-andrei-oprea <ouser99@icloud.com>
2023-08-08 15:36:21 +0300

    -fixed links again

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net.Http;
 using System.Threading.Tasks;
 using AutoUpdaterDotNET;
 
@@ -10,21 +9,17 @@ namespace TestApp
         static async Task Main(string[] args)
         {
             // generic link to latest release
-            const String latestURL = $"https://api.github.com/radu-andrei-oprea/TestAppUpdates/releases/latest/download/version.xml";
+            const String latestURL = $"https://github.com/radu-andrei-oprea/TestAppUpdates/releases/latest/download/version.xml";
 
             AutoUpdater.Start(latestURL);
 
             // print text
-            Console.WriteLine("Tenth change:");
-            const string Value = "If you see this then it works";
+            Console.WriteLine("11th change:");
+            const string Value = "fixed links again";
             Console.WriteLine(Value);
             Console.WriteLine("\nPress any key to close...");
             _ = Console.ReadKey();
             
-            
-            //AutoUpdater.Start(updateUrl);
         }
-
-       
     }
 }
